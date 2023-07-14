1d82b73bf69493466dc87b632f77a9d5306eb747
Nikita <njkita007@yandex.ru>
2023-07-14 23:09:17 +0300

    configured the font

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Button, ThemeProvider, Typography} from "@mui/material";
+import darkTheme from "./design/darkTheme";
+import '@fontsource/roboto'; // Импортируем шрифт Roboto
 
 function App() {
   return (
     <div className="App">
-      <div>О великий сайт</div>
+        <ThemeProvider theme={darkTheme}>
+            <Button type={'submit'} variant={'contained'}>Кнопка</Button>
+            <Typography>Какой-то текст</Typography>
+        </ThemeProvider>
     </div>
   );
 }
